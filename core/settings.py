3b9a11b4e183b4d5b3b66305a91f71597dac4d4d
Geovani José Pires <63720913+geovanijpires@users.noreply.github.com>
2024-04-07 12:33:45 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sis_proj',
-        'USER': 'postgresql',
+        'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
